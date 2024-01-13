8d8e2c81a6e81f2c796458c67b34c1acd4f352d2
must <mradwan@student.42abudhabi.ae>
2024-01-13 10:49:29 +0400

    c_id fixed

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -70,7 +70,7 @@ def game(request):
     return render(request, 'game.html')
 
 def authorize(request):
-    client_id = "client_id=u-s4t2ud-8156fc58fc2005216ad58258e48c1a311ceb0c4b4bd45451aba59272720501f4"
+    client_id = "client_id=u-s4t2ud-53a3167e09d6ecdd47402154ef121f68ea10b4ec95f2cb099cf3d92e56a0c822"
     redirect_uri = f"http://{request.get_host()}/callback"
 
     authorization_url = f"https://api.intra.42.fr/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
