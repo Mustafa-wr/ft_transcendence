a3b1f7f1349fea8ab8a06f2ac382ad4da7235c7a
markov199 <mkovoor2013@gmail.com>
2024-02-14 19:22:36 +0400

    html for double games

---
+++ project/staticfiles/app.js
@@ -0,0 +1,10 @@
+let autoBtn = document.querySelector(".auto-btn");
+
+function authorize() {
+  var authorizationUrl =
+    "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-53a3167e09d6ecdd47402154ef121f68ea10b4ec95f2cb099cf3d92e56a0c822&redirect_uri=https%3A%2F%2F127.0.0.1%3A8000%2Fcallback&response_type=code"; // Замените на ваш URL
+
+  window.location.href = authorizationUrl;
+}
+
+autoBtn.addEventListener("click", authorize); 
