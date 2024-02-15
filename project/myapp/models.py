23f2da83422e7d5dee68551563369114f0967e19
Mustafa Radwan <122282675+Mustafa-wr@users.noreply.github.com>
2024-02-15 12:48:17 +0000

    fixed the relationship with database issue

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -3,8 +3,11 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User
 
+def get_default_user():
+    return User.objects.first()
+
 class user_profile(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first())
+	user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_default_user)
 	login = models.CharField(max_length=100, default='default_value')
 	nickname = models.CharField(max_length=100, default='default_value')
 	email = models.EmailField(max_length=100, default='default_value')
